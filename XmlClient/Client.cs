d08d068e3ef26f1a78ae8a1cefacec1a35a79ff6
Christian Oftelund Jensen <chri9j83@edu.easj.dk>
2017-09-19 15:41:46 +0200

    Fejl i xml server input

---
+++ XmlClient/Client.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ModelLib;
+
+namespace XmlClient
+{
+    class Client
+    {
+        public void Start()
+        {
+
+            using (TcpClient socket = new TcpClient("localhost", 10002))
+            using (NetworkStream ns = socket.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            using (StreamWriter sw = new StreamWriter(ns) { AutoFlush = true })
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Car));
+                Car car = new Car();
+                car.model = "tesla";
+                car.color = "red";
+                car.registrationnr = "EL23400";
+                serializer.Serialize(sw, car);
+                sw.Flush();
+                //string line = sr.ReadLine();
+                Console.WriteLine($"{car.ToString()}");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ XmlServer/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ModelLib;
+
+namespace XmlServer
+{
+    class Server
+    {
+
+        public void Start()
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 10002);
+            server.Start();
+            while (true)
+            {
+                TcpClient socket = server.AcceptTcpClient();
+                Console.WriteLine("new client");
+                Task.Run(() =>
+                {
+                    TcpClient localSocket = socket;
+                    Console.WriteLine("Local socket");
+                    DoClient(localSocket);
+
+                });
+
+            }
+        }
+        private void DoClient(TcpClient socket)
+        {
+            
+            using (NetworkStream ns = socket.GetStream())
+            using (StreamReader sr = new StreamReader(ns))
+            //using (StreamWriter sw = new StreamWriter(ns))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Car));
+                //string line = sr.ReadLine();
+                //Console.WriteLine($"{line}");
+                Car car = new Car();
+                car = (Car)serializer.Deserialize(sr);
+                Console.WriteLine(car.ToString());
+                //if (line != String.Empty)
+                //{
+                //    sw.WriteLine("Sendt");
+                //    sw.Flush();
+                //}
+                Console.ReadLine();
+            }
+        }
+    }
+}
